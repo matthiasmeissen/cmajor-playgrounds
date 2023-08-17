d4b721caf1ad5e3a10d84e5ee7cf1b7c412ee75d
matthiasmeissen <meissenmatthias@gmail.com>
2023-08-18 00:33:12 +0200

    Completed Playground 3

--- playground-03-230817/view/index.js
+++ playground-03-230817/view/index.js
@@ -1,47 +1,45 @@
-
-
 /*
     This simple web component just manually creates a set of plain sliders for the
     known parameters, and uses some listeners to connect them to the patch.
 */
-class playground03_View extends HTMLElement
-{
-    constructor (patchConnection)
-    {
+
+class playground03_View extends HTMLElement {
+    constructor(patchConnection) {
         super();
         this.patchConnection = patchConnection;
         this.classList = "main-view-element";
         this.innerHTML = this.getHTML();
     }
 
-    connectedCallback()
-    {
-        // When the HTMLElement is shown, this is a good place to connect
-        // any listeners you need to the PatchConnection object..
+    connectedCallback() {
+        this.paramListener = (event) => {
+            // Each of our sliders has the same IDs as an endpoint, so we can find
+            // the HTML element from the endpointID that has changed:
+            const slider = this.querySelector("#" + event.endpointID);
 
-        // First, find our frequency slider:
-        const freqSlider = this.querySelector ("#frequency");
+            if (slider)
+                slider.value = event.value;
+        };
 
-        // When the slider is moved, this will cause the new value to be sent to the patch:
-        freqSlider.oninput = () => this.patchConnection.sendEventOrValue (freqSlider.id, freqSlider.value);
+        // Attach a parameter listener that will be triggered when any param is moved
+        this.patchConnection.addAllParameterListener(this.paramListener);
 
-        // Create a listener for the frequency endpoint, so that when it changes, we update our slider..
-        this.freqListener = value => freqSlider.value = value;
-        this.patchConnection.addParameterListener (freqSlider.id, this.freqListener);
+        for (const param of this.querySelectorAll(".param")) {
+            // When one of our sliders is moved, this will send the new value to the patch.
+            param.oninput = () => this.patchConnection.sendEventOrValue(param.id, param.value);
 
-        // Now request an initial update, to get our slider to show the correct starting value:
-        this.patchConnection.requestParameterValue (freqSlider.id);
+            // for each slider, request an initial update, to make sure it shows the right value
+            this.patchConnection.requestParameterValue(param.id);
+        }
     }
 
-    disconnectedCallback()
-    {
-        // When our element is removed, this is a good place to remove
-        // any listeners that you may have added to the PatchConnection object.
-        this.patchConnection.removeParameterListener ("frequency", this.freqListener);
+    disconnectedCallback() {
+        // when our element goes offscreen, we should remove any listeners
+        // from the PatchConnection (which may be shared with other clients)
+        this.patchConnection.removeAllParameterListener(this.paramListener);
     }
 
-    getHTML()
-    {
+    getHTML() {
         return `
         <style>
             .main-view-element {
@@ -62,12 +60,14 @@ class playground03_View extends HTMLElement
 
         <div id="controls">
           <p>Your GUI goes here!</p>
-          <input type="range" class="param" id="frequency" min="5" max="1000">Frequency</input>
+          <input type="range" class="param" id="frequencyIn" min="5" max="1000">Frequency</input>
+          <input type="range" class="param" id="frequency" min="5" max="1000" value="200">Filter Cutoff</input>
+          <input type="range" class="param" id="q" min="0" max="100" value="1">Resonance</input>
         </div>`;
     }
 }
 
-window.customElements.define ("playground03-view", playground03_View);
+window.customElements.define("playground03-view", playground03_View);
 
 /* This is the function that a host (the command line patch player, or a Cmajor plugin
    loader, or our VScode extension, etc) will call in order to create a view for your patch.
@@ -76,7 +76,7 @@ window.customElements.define ("playground03-view", playground03_View);
    However, this function can be `async` if you need to perform asyncronous tasks, such as
    fetching remote resources for use in the view, before completing.
 */
-export default function createPatchView (patchConnection)
-{
-    return new playground03_View (patchConnection);
+
+export default function createPatchView(patchConnection) {
+    return new playground03_View(patchConnection);
 }
